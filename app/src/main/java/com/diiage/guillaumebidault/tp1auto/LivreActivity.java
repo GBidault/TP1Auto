c684279edc14ac7abb85442eea4370970188fe8a
Guillaume Bidault <Guillaume.Bidault@diiage.org>
2017-09-20 09:40:09 +0200

    Initial commit

---
+++ app/src/main/java/com/diiage/guillaumebidault/tp1auto/LivreActivity.java
@@ -0,0 +1,13 @@
+package com.diiage.guillaumebidault.tp1auto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LivreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livre);
+    }
+}
